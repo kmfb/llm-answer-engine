08d26f48566892d37064c3ec0ef1c06db7515dfd
kmfb <zhaotianxion@gmail.com>
2024-04-28 23:06:17 +0800

    feat: add AUTH_WHITE_LISTED_EMAILS for auth

--- auth.ts
+++ auth.ts
@@ -2,4 +2,15 @@ import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+
+      const emails = process.env.AUTH_WHITE_LISTED_EMAILS?.split(",")
+
+      if (!emails?.includes(user.email as any)) {
+        return false
+      }
+      return true
+    }
+  }
 })
